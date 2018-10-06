17081cf3f4ed55a276068d3400baf82fea9f9b53
ToninoK <tonino.kastelan@skole.hr>
2018-10-06 13:32:44 +0200

    Final fixes

--- kiwi_airports.py
+++ kiwi_airports.py
@@ -7,26 +7,46 @@ req = requests.get('https://api.skypicker.com/locations?type=subentity&term=GB&l
 
 opter = Option(req.json())
 
+head = []
+data = []
+
 if '--help' in argv:
     f = open('./static/help.txt', 'r')
     msg = f.read()
     print(msg)
     f.close()
 
-if '--cities' in argv:
-    print('\n'+tabulate(opter.structure_cities(), headers=['Name', 'City'], tablefmt='orgtbl')+'\n')
-
-if '--coords' in argv:
-    print('\n'+tabulate(opter.structure_coords(), headers=['Name', 'Longitude', 'Lattitude'], tablefmt='orgtbl')+'\n')
-
 if '--iata' in argv:
-    print('\n'+tabulate(opter.structure_iata(), headers=['Iata', 'Name'], tablefmt='orgtbl')+'\n')    
+    head.append('IATA')
+    data.append(opter.structure_iata())
+
 
 if '--names' in argv:
-    print('\n'+tabulate(opter.structure_names(), headers=['Name'], tablefmt='orgtbl')+'\n')    
+    head.append('Name')
+    data.append(opter.structure_names())
+
+if '--cities' in argv:
+    head.append('City')
+    data.append(opter.structure_cities())
+
+if '--coords' in argv:
+    head.append('Longitude')
+    head.append('Lattitude')
+    for i in opter.structure_coords():
+        data.append(i)
+
 
 if '--full' in argv:
-    print('\n'+tabulate(opter.structure_full(), headers=['Iata','Name', 'City', 'Longitude', 'Lattitude'], tablefmt='orgtbl')+'\n')    
+    print('\n'+tabulate(opter.structure_full(), headers=['Iata','Name', 'City', 'Longitude', 'Lattitude'], tablefmt='orgtbl')+'\n')
+else:
+
+    reformatted_data = []
+    for i in range(len(data[0])):
+        reformatted_data.append([])
+        for j in data:
+            reformatted_data[i].append(j[i])
+
+    print('\n'+tabulate(reformatted_data, headers=head, tablefmt='orgtbl')+'\n')
 
 if len(argv)==1:
-    print('\n'+tabulate(opter.structure_option_free(), headers=['Iata','Name'], tablefmt='orgtbl')+'\n')    
+    print(tabulate(opter.structure_option_free(), headers=['Iata','Name'], tablefmt='orgtbl')+'\n')    
--- options/opts.py
+++ options/opts.py
@@ -19,60 +19,52 @@ class Option:
         """Function which returns a list of data for the airports
 
         Returns:
-            list -- contains airport data; name, city
+            airport_cities_data -- contains airport data; name, city
         """
-
-        lst = []
+        airport_cities_data = []
         for airport in self.data:
-            airport_cities_data = []
-            airport_cities_data.append(airport['name'])
             airport_cities_data.append(airport['city']['name'])
-            lst.append(airport_cities_data)
-        return lst
+        return airport_cities_data
 
     def structure_coords(self):
         """Function which returns a list of data for the airports
 
         Returns:
-            list -- contains airport data; name, longitude, lattitude
+            lst -- contains airport data; longitude, lattitude
         """
     
         lst = []
+        airport_longitudes = []
+        airport_latitudes = []
         for airport in self.data:
-            airport_cities_data = []
-            airport_cities_data.append(airport['name'])
-            airport_cities_data.append(airport['location']['lon'])
-            airport_cities_data.append(airport['location']['lat'])
-            lst.append(airport_cities_data)
+            airport_longitudes.append(airport['location']['lon'])
+            airport_latitudes.append(airport['location']['lat'])
+        lst.append(airport_longitudes)
+        lst.append(airport_latitudes)
         return lst
 
     def structure_iata(self):
         """Function which returns a list of data for the airports
 
         Returns:
-            list -- contains airport data; iata code, name
+            airport_iata_data -- contains airport data; iata code
         """
-        lst = []
+        airport_iata_data = []
         for airport in self.data:
-            airport_cities_data = []
-            airport_cities_data.append(airport['code'])
-            airport_cities_data.append(airport['name'])
-            lst.append(airport_cities_data)
-        return lst
+            airport_iata_data.append(airport['code'])
+        return airport_iata_data
     
     def structure_names(self):
         """Function which returns a list of data for the airports
 
         Returns:
-            list -- contains airport data; name
+            airport_name_data -- contains airport data; name
         """
 
-        lst = []
+        airport_name_data = []
         for airport in self.data:
-            airport_cities_data = []
-            airport_cities_data.append(airport['name'])
-            lst.append(airport_cities_data)
-        return lst
+            airport_name_data.append(airport['name'])
+        return airport_name_data
     
     def structure_full(self):
         """Function which returns a list of data for the airports
@@ -83,26 +75,26 @@ class Option:
 
         lst = []
         for airport in self.data:
-            airport_cities_data = []
-            airport_cities_data.append(airport['code'])
-            airport_cities_data.append(airport['name'])
-            airport_cities_data.append(airport['city']['name'])
-            airport_cities_data.append(airport['location']['lon'])
-            airport_cities_data.append(airport['location']['lat'])
-            lst.append(airport_cities_data)
+            airport_full_data = []
+            airport_full_data.append(airport['code'])
+            airport_full_data.append(airport['name'])
+            airport_full_data.append(airport['city']['name'])
+            airport_full_data.append(airport['location']['lon'])
+            airport_full_data.append(airport['location']['lat'])
+            lst.append(airport_full_data)
         return lst
     
     def structure_option_free(self):
         """Function which returns a list of data for the airports
 
         Returns:
-            list -- contains airport data; iata code, name
+            airport_optfree_data -- contains airport data; iata code, name
         """
 
         lst = []
         for airport in self.data:
-            airport_cities_data = []
-            airport_cities_data.append(airport['code'])
-            airport_cities_data.append(airport['name'])
-            lst.append(airport_cities_data)
+            airport_optfree_data = []
+            airport_optfree_data.append(airport['code'])
+            airport_optfree_data.append(airport['name'])
+            lst.append(airport_optfree_data)
         return lst
